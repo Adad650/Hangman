bccb2c06d62edbaa734a4f9fa1c67edba87f67db
adi <adi.takhar@gmail.com>
2025-04-21 18:51:27 -0400

    Pseudo code

--- hangman.py
+++ hangman.py
@@ -1,1 +1,32 @@
-print ("Hello World")
+# Choose a word bewtween 2 - 8 letters long.
+
+
+
+# Create a list of letters by taking each letter from the word and putting it in the list one by one. Name this list actualWord
+
+# create a list that is the same length as the word but filled with underscores. Name this list secretWord
+
+# Initialize all required variables
+# A int varaible with 6 in it to keep track of the number of lives. "Lives = 6"
+
+
+# The user puts it there first guess. Check the length of the users guess to ensure that it is 1 letter
+
+# Make a for loop to compare the letter that the user guessed to each letter in the list
+## Within the for loop, do the following:
+## Run the for loop for the length of the list
+## Compare the character that the user guessed to each items in the list actualWord. 
+## Cycle through the actualWord list using the iteration variable in the for loop
+## if the character that the user typed is the same as the character in the actualWord at that location, If the userInput == actualWord[i], Then secretWord[i] = userInput
+## Compare secretWord to actualWord
+## When they are the same Break from the for loop  
+## Break when the user runs out of guesses (i.e. when Lives = 0)
+## If user is incorrect Lives -= 1
+## Print secretWord again and lives left
+
+
+#If the user fails to guess the word, computerPermissionToBullyUser = True
+
+
+#
+       
